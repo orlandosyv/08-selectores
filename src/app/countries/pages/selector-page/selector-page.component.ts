fa4f6f7649add348111b14b2f00183be3ce5400e
Orlando YV <sergioyv@gmail.com>
2024-11-24 22:30:30 -0500

    regions selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -10,7 +12,12 @@ export class SelectorPageComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-  ) {}
+    private countriesService: CountriesService
+  ) { }
+
+  get regions(): Region[] {
+    return this.countriesService.regions
+  }
 
   public myForm: FormGroup = this.formBuilder.group({
     region: ['', Validators.required],
