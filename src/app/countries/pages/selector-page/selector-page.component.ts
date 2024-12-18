d99d587b4b6f2109b93ebcf241139dc250eeab06
Orlando YV <sergioyv@gmail.com>
2024-12-17 22:11:52 -0500

    clean country when region changes

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -35,11 +35,12 @@ export class SelectorPageComponent implements OnInit {
   onRegionChanged(): void {
     this.myForm.get("region")!.valueChanges
       .pipe(
-          switchMap(region => this.countriesService.getCountriesByRegion(region))
-        )
-        .subscribe(countries => {
-          this.countriesByRegion = countries;
-      })
+        tap(() => this.myForm.get('country')!.setValue('')),
+        switchMap((region) => this.countriesService.getCountriesByRegion(region)),
+      )
+      .subscribe(countries => {
+        this.countriesByRegion = countries;
+      });
   }
 
 }
